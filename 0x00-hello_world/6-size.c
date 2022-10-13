cb483195ac375984102414c46fa0a40669c98668
OTH143 <toyintaiwohammed@gmail.com>
2022-10-13 16:02:47 -0700

    do not forget about the extra

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
-
+/**
+ * main - Ent
+ * Return: always 0
+ */
 int main(void)
 {
 	printf("size of char: %li byte(s)\n", sizeof(char));
@@ -7,5 +10,6 @@ int main(void)
 	printf("size of int: %li byte(s)\n", sizeof(int));
 	printf("size of long int: %li byte(s)\n", sizeof(long int));
 	printf("size of double: %li byte(s)\n", sizeof(double));
+	printf("\"size of long long int: %li bytes)\n", sizeof(long long int));
 	return (0);
 }
