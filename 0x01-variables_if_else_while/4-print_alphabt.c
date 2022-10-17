41575ce6c4f8f1acb1e9201653694804794fc79f
OTH143 <toyintaiwohammed@gmail.com>
2022-10-16 18:46:36 -0700

    i wanna sleep

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,21 +1,20 @@
-#include <stdio.>
+#include <stdio.h>
 
 /**
  * main - entry
  * Return:always 0
- * followed by a new line except e and q
  */
 int main(void)
 {
 	char ls;
 
-	for (ls 'a'; ls <= 'z'; ls++)
+	for (ls = 'a'; ls <= 'z'; ls++)
 	{
-		if (ls : = 'e' && la : = 'q')
+		if (ls != 'e' && ls != 'q')
 			putchar(ls);
 	}
 
-	putchar(\n);
+	putchar('\n');
 
 	return (0);
 }
