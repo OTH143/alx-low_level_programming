69b838229ec294abc100fcde9cc8e8b0dd8b39f2
OTH143 <toyintaiwohammed@gmail.com>
2022-10-16 18:13:38 -0700

    it is literally 2am

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.>
+
+/**
+ * main - entry
+ * Return:always 0
+ * followed by a new line except e and q
+ */
+int main(void)
+{
+	char ls;
+
+	for (ls 'a'; ls <= 'z'; ls++)
+	{
+		if (ls : = 'e' && la : = 'q')
+			putchar(ls);
+	}
+
+	putchar(\n);
+
+	return (0);
+}
