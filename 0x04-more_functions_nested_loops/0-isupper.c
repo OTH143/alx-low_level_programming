2ea8b1f1917330d8d9518dd48153ab64281d0434
OTH143 <toyintaiwohammed@gmail.com>
2022-10-21 09:06:25 -0700

    ALX

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _isupper - Checks for uppercase characters
+ * @c: The charcter to be checked
+ *
+ * Return: 1 if c is uppercase, otherwise 0
+ */
+int _isupper(int c)
+{
+	int c;
+
+	if (c > 'A' && c < 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
