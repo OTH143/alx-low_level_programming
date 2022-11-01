6cf96d1667e0537646181a387670da012ceff124
OTH143 <toyintaiwohammed@gmail.com>
2022-11-01 05:24:36 -0700

    g

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _memcpy - it copies n bytes
+ * @dest: param 1
+ * @src: param 2
+ * @n: integer
+ * Return: dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	return (memcpy(dest));
+}
