1d41b939f4f50f41a829ddddeec0f0dd68483e1e
OTH143 <toyintaiwohammed@gmail.com>
2022-11-01 06:20:04 -0700

    whooppyy!!

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * print_chessboard - function to print character
+ * @a: character to be printed
+ * Return:0
+ */
+void print_chessboard(char (*a)[8])
+{
+	return (print_chessboard(a));
+}
---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_diagsums - prints an integer to stdout
+ * @a:integer
+ * @size:integer
+ * Return:0
+ */
+void print_diagsums(int *a, int size)
+{
+	return (print_diagsums(a, size));
+}
