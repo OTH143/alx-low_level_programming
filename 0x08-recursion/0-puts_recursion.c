782f6151411b9719b408511341fc5d3945faa78f
OTH143 <toyintaiwohammed@gmail.com>
2022-11-03 12:09:48 -0700

    d

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,4 +8,6 @@
 void _puts_recursion(char *s)
 {
 	_puts_recursion(s);
+	_puts_recursion(*s);
+	putchar("");
 }
---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - prints a string in reverse
+ * @s: string to be printed
+ * Return: No return
+ */
+void _print_rev_recursion(char *s)
+{
+	_print_rev_recursion(s);
+}
