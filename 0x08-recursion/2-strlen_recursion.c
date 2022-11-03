8ed6e938c57f9ea0b2b4280335e15728199cc153
OTH143 <toyintaiwohammed@gmail.com>
2022-11-03 12:14:52 -0700

    k

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - prints the length of a string
+ * @s: the string to be printed
+ * Return:0
+ */
+int _strlen_recursion(char *s)
+{
+	_strlen_recursion(s);
+}
