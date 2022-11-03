c1f0f0c22a4f9f2f508add204d3fa40f1ed298a8
OTH143 <toyintaiwohammed@gmail.com>
2022-11-03 13:44:09 -0700

    k

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * factorial - prints the factorial of a given number
+ * @n: the integer to be printed
+ * Return: on success 0
+ */
+int factorial(int n)
+{
+	return (factorial(4));
+}
