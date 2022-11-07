41ccc949bd15696cfc9493a976bcbeb6cb576672
OTH143 <toyintaiwohammed@gmail.com>
2022-11-07 05:38:15 -0800

    l

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char *argv[])
 {
-	int v;
-	int y;
-	int i;
+	int v, y, i;
 
-	if (argc != 1)
+	if (argc != 3)
 	{
 		printf("Error\n");
 			return (1);
 	}
-	printf("input two numbers here:\n");
-	scanf("%d %d", &v, &y);
-	i = (v * y);
+	y = atoi(argv[1]);
+	v = atoi(argv[2]);
+	i = v * y;
+	
 	printf("The multiplication of %d and %d is %d\n", v, y, i);
 	return (0);
 }
