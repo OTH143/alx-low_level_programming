4a3a665c498bad898e8de23c04fcb7d23e745180
OTH143 <toyintaiwohammed@gmail.com>
2022-11-07 03:45:15 -0800

    on the road again

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - print input to output
+ * @argc: number of command statements
+ * @argv: name of command statements
+ * Return: 0
+ */
+int main(int argc, char *argv)
+{
+	printf("%d\n", argc);
+	return (0);
+}
