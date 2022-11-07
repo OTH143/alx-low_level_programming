3ee2622e9d78c726a5a6d7d07c502dd5bec979f1
OTH143 <toyintaiwohammed@gmail.com>
2022-11-07 03:14:10 -0800

    l

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints out standard input to standard output
+ * @argc: prints out the number of commands
+ * @argv: prints out what is in argc
+ * Return: 0
+ */
+int main(char argc, int argv[])
+{
+	printf("%d\n", argc);
+	if (argc > 0)
+	{
+		printf("0-whatsmyname.c\n");
+		printf("\n");
+	}
+	return (0);
+}
