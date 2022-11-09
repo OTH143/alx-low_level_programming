cab78d465656059cca6d7d27e3cc6165950db410
OTH143 <toyintaiwohammed@gmail.com>
2022-11-09 15:21:54 -0800

    l

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,9 +1,8 @@
 #include "main.h"
-#include <stddef.h>
 #include <stdlib.h>
 
 /**
- * *create_array - array for prints a string
+ * *create_array - array for printing a string
  * @size: parameter 1
  * @c: parameter 2
  * Return: pointer
@@ -15,9 +14,8 @@ char *create_array(unsigned int size, char c)
 
 	buffer = malloc(sizeof(char) * size);
 	if (size == 0 || buffer == NULL)
-	{
 		return (NULL);
-	}
+
 	for (tn = 0; tn < size; tn++)
 		buffer[tn] = c;
 		return (buffer);
