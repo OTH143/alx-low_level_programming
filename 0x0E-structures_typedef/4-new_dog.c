dcc38393fe8718eac85262120d2aaf8bb7239a5a
OTH143 <toyintaiwohammed@gmail.com>
2022-11-15 13:57:31 -0800

    s

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,22 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * new_dog - creates a new dog
+ * @name: param 1
+ * @age: param 2
+ * @owner: param 3
+ * Return: NULL if function fails
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	strcpy new_dog->name;
+	strcpy new_dog->owner;
+	if (strcpy != new_dog->(name; owner)
+	return (NULL);
+	else
+	return (0);
+
+	return (0);
+}
